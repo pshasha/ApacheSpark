3d6fb2ab93179d2ee33b8b3ef9d5647df4bc71a3
shashank <shashankpathak9890@gmail.com>
2019-01-17 12:33:30 +0530

    define the csv datasources for the dataframes

--- src/main/scala/retail_db/dailyRevenueusingDF.scala
+++ src/main/scala/retail_db/dailyRevenueusingDF.scala
@@ -36,7 +36,7 @@ object dailyRevenueusingDF {
     val path = args(1)
 
     val orders = sqlContext.read.schema(ordersSchema).
-      format("csv").load(s"${path}/orders")
+      format("com.databricks.spark.csv").load(s"${path}/orders")
 
     val order_items = sqlContext.read.schema(orderItemsSchema).format("com.databricks.spark.csv").load(s"${path}/order_items")
 
